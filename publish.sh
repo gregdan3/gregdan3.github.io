4769e05ce03041b9962b47a1bfd952184b8d6901
Gregory Danielson <gregory.danielson3@gmail.com>
2022-06-15 21:19:20 -0500

    Add publish script for github pages

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+make clean all
+git checkout -f gh-pages
+git rm -rf ./*
+mv build/* .
+rm -rf build/
+git add .
+git commit --amend --date=now -m "Update site"
+git push --force
+git checkout -f main
