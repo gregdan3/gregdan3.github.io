d2367baccd2ef9127004cb2f5eef6e1e36c582f9
Gregory Danielson <gregory.danielson3@gmail.com>
2023-11-09 16:39:54 -0600

    feat: minor cleanup of BlogEntry
    - combined h-56 for mobile and desktop
    - opacity for tags
    - margin for tags
    - border under image and gradient
    - corrections for image bounds/text

--- src/lib/components/BlogEntry.svelte
+++ src/lib/components/BlogEntry.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
 	import { formatBlogPostDate } from '$lib/utils';
-	import { onMount } from 'svelte';
 
 	import BlogTag from '$lib/components/BlogTag.svelte';
 
 	export let slug: string;
 	export let title: string;
-	export let author: string;
+	// export let author: string;
 	export let description: string;
 	export let date: string;
-	export let published: boolean;
+	// export let published: boolean;
 	export let image: string = '';
-	export let updated: string | undefined;
+	// export let updated: string | undefined;
 	export let tags: string[] = [];
 
 	let tagsContainer: HTMLElement;
@@ -19,27 +18,26 @@
 
 <a
 	href={`/blog/${slug}`}
-	class="relative h-48 md:h-52 bg-table dark:bg-table-dark hover:bg-ends hover:dark:bg-ends-dark transition-all ease-in-out duration-300 rounded-lg shadow-md border border-accent dark:border-ctrim-dark"
+	class="not-prose relative h-56 bg-table dark:bg-table-dark hover:bg-ends hover:dark:bg-ends-dark transition-all ease-in-out duration-300 rounded-lg shadow-md border border-accent dark:border-ctrim-dark"
 >
-	>
 	{#if image}
 		<img
 			src={image}
 			alt="blog-banner"
-			class="absolute top-0 left-0 w-full h-3/4 rounded-t-lg object-cover"
+			class="absolute top-0 left-0 w-full h-2/3 rounded-t-lg object-cover border-b border-accent dark:border-ctrim-dark"
 		/>
 	{:else}
 		<div
-			class="absolute top-0 left-0 w-full h-2/3 bg-gradient-to-br from-table to-accent dark:from-table-dark dark:to-trim-dark rounded-t-lg"
+			class="absolute top-0 left-0 w-full h-2/3 bg-gradient-to-br from-table to-accent dark:from-table-dark dark:to-trim-dark rounded-t-lg border-b border-accent dark:border-ctrim-dark"
 		></div>
 	{/if}
 
 	<div class="absolute top-4 left-4">
 		{#if tags}
-			<div class="flex flex-wrap space-x-0.5">
+			<div class="flex flex-wrap space-x-0.5 m-0.5">
 				{#each tags as tag}
 					<span
-						class="text-xs bg-ends border border-trim dark:bg-ends-dark dark:border-trim-dark m-0.5 p-0.5 px-1 rounded"
+						class="text-xs bg-ends border border-trim dark:bg-ends-dark dark:border-trim-dark m-0.5 p-0.5 px-1 rounded opacity-80 dark:opacity-70"
 						>{tag}</span
 					>
 				{/each}
