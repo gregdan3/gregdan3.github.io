ac2964e41086b8c9e0839b64fcd44c1fbbf2b242
Gregory Danielson <gregory.danielson3@gmail.com>
2023-11-08 22:56:00 -0600

    Make meta more clever / clean by calculating/making editable more elements

--- src/lib/components/Meta.svelte
+++ src/lib/components/Meta.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
-	export let title: string;
-	export let description = "read because it's free";
-
+	import avatar from '$lib/assets/avatar.jpg';
 	import { page } from '$app/stores';
+
+	export let title: string = '';
+	export let description = "read because it's free";
+	export let image: string = avatar;
+
+	const locale = 'en_US';
+	const site_name = 'gregsite';
+	const href = 'https://gregdan3.dev';
+
+	let titleFull: string;
+	if (title) {
+		titleFull = `${title} | ${site_name}`;
+	} else {
+		titleFull = `${site_name}`;
+	}
 </script>
 
 <svelte:head>
@@ -11,15 +24,15 @@
 		name="viewport"
 		content="width=device-width, height=device-height, user-scalable=yes, initial-scale=1.0"
 	/>
-	<meta property="og:title" content="gregsite - {title}" />
+	<meta property="og:title" content={titleFull} />
 	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://gregdan3.dev{$page.url.pathname}" />
-	<meta property="og:image" content="/avatar.jpg" />
-	<meta property="og:locale" content="en_US" />
-	<meta property="og:site_name" content="gregsite" />
+	<meta property="og:url" content="{href}{$page.url.pathname}" />
+	<meta property="og:image" content={image} />
+	<meta property="og:locale" content={locale} />
+	<meta property="og:site_name" content={site_name} />
 	<meta property="og:description" content={description} />
 
-	<title>gregsite - {title}</title>
-	<link href="https://gregdan3.dev/" rel="canonical" />
+	<title>{titleFull}</title>
+	<link {href} rel="canonical" />
 	<link href="/favicon.ico" rel="icon" type="image/png" />
 </svelte:head>
