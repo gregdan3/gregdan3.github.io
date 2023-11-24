f6f701fe1bad4b2e66b942d733896f30b22c7730
Gregory Danielson <gregory.danielson3@gmail.com>
2023-11-24 08:51:32 -0600

    feat: handle failure cases in discord server endpoint

--- src/routes/discord/+server.ts
+++ src/routes/discord/+server.ts
@@ -2,9 +2,19 @@ import { json } from '@sveltejs/kit';
 
 const LANYARD_URL = 'https://api.lanyard.rest/v1';
 
+export const GET = async ({ request }: { request: Request }) => {
+	return json({
+		error: 'GET method not allowed',
+		expected: { method: 'POST', parameters: { user_id: 'number' } }
+	});
+};
+
 export const POST = async ({ request }: { request: Request }) => {
 	const postData = await request.json();
 	const user_id = postData.user_id;
 	const data = await fetch(`${LANYARD_URL}/users/${user_id}`).then((res) => res.json());
-	return json(data.data);
+	if (!data.error) {
+		return json(data.data);
+	}
+	return json(data); // pass through lanyard error
 };
