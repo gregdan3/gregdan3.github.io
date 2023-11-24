19d92bcf283f7b3db1d34ee05af426114fbc4aa6
Gregory Danielson <gregory.danielson3@gmail.com>
2023-11-23 23:43:27 -0600

    feat: lanyard types
    referenced
    https://github.com/malloryhayr/svelte-lanyard/blob/main/src/types.ts
    
    however the lanyard data was notably inaccurate and out of date in several places

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -41,3 +41,74 @@ export interface Project {
 	skills: Technology[];
 	header?: string;
 }
+
+export interface LanyardData {
+	spotify: LanyardSpotifyData | null;
+	kv: { [key: string]: string };
+	listening_to_spotify: boolean;
+	discord_user: LanyardDiscordUser;
+	discord_status: 'online' | 'idle' | 'dnd' | 'offline';
+	activities: LanyardDiscordActivity[];
+	active_on_discord_mobile: boolean;
+	active_on_discord_desktop: boolean;
+}
+
+export interface LanyardTimestamps {
+	start: number;
+	end: number;
+}
+
+export interface LanyardSpotifyData {
+	track_id: string;
+	timestamps: LanyardTimestamps;
+	song: string;
+	artist: string;
+	album_art_url: string;
+	album: string;
+}
+
+export interface LanyardDiscordUser {
+	id: number;
+	username: string;
+	avatar: string;
+	discriminator: string;
+	bot: boolean;
+	global_name: string;
+	avatar_decoration_data: string;
+	display_name: string;
+	public_flags: number;
+}
+
+export interface LanyardDiscordEmoji {
+	id: number;
+	name: string;
+	animated: boolean;
+}
+
+export interface LanyardDiscordParty {
+	id: string;
+}
+
+export interface LanyardDiscordAssets {
+	small_text: string;
+	small_image: string;
+	large_text: string;
+	large_image: string;
+}
+
+export interface LanyardDiscordActivity {
+	type: number;
+	state: string;
+	name: string;
+	id: string;
+	emoji?: LanyardDiscordEmoji;
+	created_at: number;
+	timestamps?: LanyardTimestamps;
+	sync_id?: string;
+	session_id?: string;
+	party?: LanyardDiscordParty;
+	flags?: number;
+	details?: string;
+	assets?: LanyardDiscordAssets;
+	application_id?: number;
+}
