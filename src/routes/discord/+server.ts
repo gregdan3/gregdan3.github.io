e9fb7ccc7a6887327a7b88a29b4d80c6cc263f81
Gregory Danielson <gregory.danielson3@gmail.com>
2023-11-23 23:41:02 -0600

    fix: Use user_id from request, return data.data from discord endpoint
    Matches types in types.ts later on

--- src/routes/discord/+server.ts
+++ src/routes/discord/+server.ts
@@ -1,9 +1,10 @@
 import { json } from '@sveltejs/kit';
 
 const LANYARD_URL = 'https://api.lanyard.rest/v1';
-const MY_USER_ID = '497549183847497739';
 
-export const GET = async ({ fetch }) => {
-	const data = await fetch(`${LANYARD_URL}/users/${MY_USER_ID}`).then((res) => res.json());
-	return json(data);
+export const POST = async ({ request }: { request: Request }) => {
+	const postData = await request.json();
+	const user_id = postData.user_id;
+	const data = await fetch(`${LANYARD_URL}/users/${user_id}`).then((res) => res.json());
+	return json(data.data);
 };
