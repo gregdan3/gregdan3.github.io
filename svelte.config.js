eb87a7f24e774e85300212e8be2887322ad03d43
Gregory Danielson <gregory.danielson3@gmail.com>
2024-02-29 14:06:11 -0600

    fix: Switch back to vercel adapter (I guess)

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,9 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
 // import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 // import { Config } from '@sveltejs/kit';
 
