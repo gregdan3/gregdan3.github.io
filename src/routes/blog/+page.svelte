13e6979af010311f4d416a6ee7876c31e28d696d
Gregory Danielson <gregory.danielson3@gmail.com>
2024-02-29 13:47:26 -0600

    feat: Rework blog to static endpoint

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { fetchSlugFromPath } from '$lib/utils';
 	import { goto } from '$app/navigation';
 	import BlogEntry from '$lib/components/BlogEntry.svelte';
 	import { Pagination } from 'flowbite-svelte';
@@ -8,15 +9,18 @@
 	import { buildBlogPageLinks } from '$lib/utils';
 	import Meta from '$lib/components/Meta.svelte';
 
-	export let data: PageData;
-
 	const metadata = {
 		title: 'Blog',
 		description: 'Every dev needs an abandoned blog.'
 	};
 
+	const modules = import.meta.glob(`/src/data/posts/*.{md,svelte.md}`);
+	const POSTS_PER_PAGE = 6;
+	$: totalPages = 1;
+	$: pages = buildBlogPageLinks(totalPages);
 	$: currentPage = Number($page.url.searchParams.get('page')) || 1;
-	let pages = buildBlogPageLinks(data.totalPages);
+	// TODO: if user goes directly to a page that doesn't exist, they get an empty page
+	// TODO: clicking a page link from that page refreshes the page, but should not
 
 	$: {
 		pages.forEach((page) => {
@@ -29,15 +33,58 @@
 		pages = pages;
 	}
 
-	const previous = () => {
-		currentPage -= 1;
-		if (1 > currentPage || currentPage > data.totalPages) currentPage = 1;
+	export const transformPosts = async () => {
+		const postPromises = Object.entries(modules).map(([path, resolver]) =>
+			resolver().then(
+				(post) =>
+					({
+						slug: fetchSlugFromPath(path),
+						...(post as App.MdsvexFile).metadata
+					}) as App.BlogPost
+			)
+		);
+
+		let posts = await Promise.all(postPromises);
+		posts = posts.filter((post) => post.published);
+		posts.sort((a, b) => (new Date(a.date) > new Date(b.date) ? -1 : 1));
+
+		// we have to fetch only published posts, but can't know which are published until now
+		totalPages = Math.ceil(Object.keys(posts).length / POSTS_PER_PAGE);
+
+		const firstPostIndex = (currentPage - 1) * POSTS_PER_PAGE;
+		const lastPostIndex = firstPostIndex + POSTS_PER_PAGE;
+		const pagePosts = posts.slice(firstPostIndex, lastPostIndex);
+		return pagePosts;
+	};
+
+	$: posts = transformPosts();
+
+	const updateCurrentPage = (page: number) => {
+		currentPage = Math.min(Math.max(page, 1), totalPages); // force bounds
 		goto(`/blog?page=${currentPage}`);
 	};
+
+	const previous = () => {
+		let oldCurrent = currentPage;
+		updateCurrentPage(currentPage - 1);
+		if (currentPage == oldCurrent) {
+			return;
+		}
+
+		posts = transformPosts();
+	};
 	const next = () => {
-		currentPage += 1;
-		if (1 > currentPage || currentPage > data.totalPages) currentPage = data.totalPages;
-		goto(`/blog?page=${currentPage}`);
+		let oldCurrent = currentPage;
+		updateCurrentPage(currentPage + 1);
+		if (currentPage == oldCurrent) {
+			return;
+		}
+
+		posts = transformPosts();
+	};
+
+	const refreshPosts = () => {
+		posts = transformPosts();
 	};
 </script>
 
@@ -47,9 +94,14 @@
 
 <div class="grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-4 pb-16">
 	<!-- NOTE: pb is to make space for Pagination -->
-	{#each data.posts as post}
-		<BlogEntry {post} />
-	{/each}
+	{#await posts}
+		<div></div>
+		<!-- TODO: loading standin -->
+	{:then resolvedPosts}
+		{#each resolvedPosts as post}
+			<BlogEntry {post} />
+		{/each}
+	{/await}
 </div>
 
 <div class="not-prose absolute bottom-12 left-1/2 right-1/2">
@@ -58,6 +110,7 @@
 		{pages}
 		on:previous={previous}
 		on:next={next}
+		on:click={refreshPosts}
 		large
 		icon
 		ariaLabel="Blog Navigation"
